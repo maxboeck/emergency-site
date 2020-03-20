eaf43f18d82a5fd83441127bde91fe1255fb9143
Max Böck <hello@mxb.at>
2020-03-20 18:09:44 +0100

    add admin preview template

---
+++ admin/templates/index.js
@@ -0,0 +1,4 @@
+import Post from '/admin/templates/post.js'
+
+CMS.registerPreviewTemplate('posts', Post)
+CMS.registerPreviewStyle('/assets/css/style.css')
---
+++ admin/templates/post.js
@@ -0,0 +1,29 @@
+import htm from 'https://unpkg.com/htm?module'
+import format from 'https://unpkg.com/date-fns@2.7.0/esm/format/index.js?module'
+
+const html = htm.bind(h)
+
+// Preview component for a Post
+const Post = createClass({
+    render() {
+        const entry = this.props.entry
+        return html`
+            <main>
+                <article class="post">
+                    <header>
+                        <h1>${entry.getIn(['data', 'title'], null)}</h1>
+                        <time
+                            >${format(
+                                entry.getIn(['data', 'date'], new Date()),
+                                'dd LLLL yyyy'
+                            )}</time
+                        >
+                    </header>
+                    ${this.props.widgetFor('body')}
+                </article>
+            </main>
+        `
+    }
+})
+
+export default Post
