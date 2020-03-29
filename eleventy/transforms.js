bdb8808fb85e970a53ea058e6f8c6394754e47c3
James Edmonds <james@deckchairlabs.com>
2020-03-29 17:45:11 +1030

    switched to htmlnano to allow purging unused css

--- eleventy/transforms.js
+++ eleventy/transforms.js
@@ -1,18 +1,31 @@
-const htmlmin = require('html-minifier')
+const htmlnano = require('htmlnano')
+
+const preset = Object.assign(htmlnano.presets.safe, {
+    collapseWhitespace: 'all',
+    removeComments: 'all',
+    removeRedundantAttributes: true
+})
 
 module.exports = {
-    htmlmin: function(content, outputPath) {
+    htmlmin: async function(content, outputPath) {
         if (
             outputPath &&
             outputPath.endsWith('.html') &&
             process.env.NODE_ENV === 'production'
         ) {
-            return htmlmin.minify(content, {
-                useShortDoctype: true,
-                removeComments: true,
-                collapseWhitespace: true
-            })
+            const { html } = await htmlnano.process(
+                content,
+                {
+                    removeUnusedCss: {
+                        tool: 'purgeCSS'
+                    }
+                },
+                preset
+            )
+
+            return html
         }
+
         return content
     }
 }
