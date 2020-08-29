fdf3ff9409a0a84c05f1234ccc0a67fa34de0b80
Max Böck <hello@mxb.at>
2020-08-29 18:03:47 +0200

    rm worker state check

--- src/static/scripts/offline.js
+++ src/static/scripts/offline.js
@@ -59,10 +59,8 @@ function refresh() {
     if (isRefreshing) {
         return
     }
-    if (worker.state === 'activated') {
-        isRefreshing = true
-        window.location.reload()
-    }
+    isRefreshing = true
+    window.location.reload()
 }
 
 // Build an display a message to announce an updated Service Worker
